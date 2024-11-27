de2d49597ac1b9c8eab28b420636c20adacfc915
unknown <iliyas.omarov27@gmail.com>
2024-11-27 22:15:32 +0500

    изменен

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-// // One
-// const daytime = prompt("Какое сейчас время суток? 'Утро', 'День' или 'Вечер'", "")
+// One
+const daytime = prompt("Какое сейчас время суток? 'Утро', 'День' или 'Вечер'", "")
 
-// if (typeof daytime === "string" && daytime) {
-//     if (daytime == "Утро") {
-//         alert("Предлогаю вам выпить кофе.")
-//     } else if (daytime == "День") {
-//         alert("Предлогаю вам попить чаю.") 
-//     } else if (daytime == "Вечер") {
-//         alert("Ваш вечер скпасит кружечка горячего шоколада.")
-//     } else {
-//         alert("Пожалуйста, напишите: 'Утро', 'День' или 'Вечер'.")
-//     }
-// } else {
-//     alert("Вы немного ошиблись. Пожалуйста, напишите: 'Утро', 'День' или 'Вечер'.")
-// }
-// // Two
-// const questions = ['Ваше имя?', 'Ваш возраст?', 'Ваш предмет?', 'Ваша оценка?'];
-// let answers = []
-// for (let i = 0; i < questions.length; i++) {
-//     let answer = prompt(questions[i], "")
-//     answers.push(answer)
-// }
-// const name = answers[0]
-// console.log(name)
-// const age = +answers[1] + 5
-// console.log(age)
-// const subject = answers[2]
-// console.log(subject)
-// const grade = answers[3]
-// console.log(grade)
+if (typeof daytime === "string" && daytime) {
+    if (daytime == "Утро") {
+        alert("Предлогаю вам выпить кофе.")
+    } else if (daytime == "День") {
+        alert("Предлогаю вам попить чаю.") 
+    } else if (daytime == "Вечер") {
+        alert("Ваш вечер скпасит кружечка горячего шоколада.")
+    } else {
+        alert("Пожалуйста, напишите: 'Утро', 'День' или 'Вечер'.")
+    }
+} else {
+    alert("Вы немного ошиблись. Пожалуйста, напишите: 'Утро', 'День' или 'Вечер'.")
+}
+// Two
+const questions = ['Ваше имя?', 'Ваш возраст?', 'Ваш предмет?', 'Ваша оценка?'];
+let answers = []
+for (let i = 0; i < questions.length; i++) {
+    let answer = prompt(questions[i], "")
+    answers.push(answer)
+}
+const name = answers[0]
+console.log(name)
+const age = +answers[1] + 5
+console.log(age)
+const subject = answers[2]
+console.log(subject)
+const grade = answers[3]
+console.log(grade)
 // Three
 const movies = [
     { title: "Начало", director: "Кристофер Нолан", year: 2010, genre: "фантастика" },
